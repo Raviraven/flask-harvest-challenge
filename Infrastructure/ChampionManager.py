0f7546fa992f706b33a825f539ae06e1e8d5b719
Michał Kruszewski <kruszewski2@gmail.com>
2019-12-22 20:46:38 +0100

    added getting id to champion manager script

--- Infrastructure/ChampionManager.py
+++ Infrastructure/ChampionManager.py
@@ -2,6 +2,8 @@ championsDB = []
 
 
 def add_champion(new_champion):
+    if new_champion.id is None or new_champion.id <= 0:
+       new_champion.id = get_champion_id()
     championsDB.append(new_champion)
 
 
@@ -21,3 +23,11 @@ def assign_values(champion_from_db, new_champion):
     champion_from_db.souls = new_champion.souls
     champion_from_db.stats = new_champion.stats
     champion_from_db.game_time = new_champion.game_time
+
+
+def get_champion_id():
+    minId = 0
+    for champ in championsDB:
+        if champ.id < minId:
+            minId = champ.id
+    return minId
