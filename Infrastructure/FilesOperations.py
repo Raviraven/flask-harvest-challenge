8df5ebb3496a230d004287ac0d368a3c59090c02
Michał Kruszewski <kruszewski2@gmail.com>
2019-12-22 19:46:44 +0100

    Added file operations

---
+++ Infrastructure/FilesOperations.py
@@ -0,0 +1,28 @@
+from Models.Champion import parse_dictionary_to_champion
+from Infrastructure.ChampionManager import add_champion
+from Infrastructure.ChampionManager import get_champions
+import json
+
+filename = "HarvestChallenge.txt"
+
+
+def read_data_from_file():
+    try:
+        f = open(filename, "r")
+        for champion in f.readlines():
+            loaded_champion = parse_dictionary_to_champion(champion)
+            add_champion(loaded_champion)
+        f.close()
+    except Exception as error:
+        print("Some error occured during reading from file: {0}".format(error))
+
+
+def save_data_to_file():
+    try:
+        f=open(filename, "a")
+        for champion in get_champions():
+            champStr = json.dumps(champion.get_champion_parsed_as_dictionary()) + "\n"
+            f.write(champStr)
+        f.close()
+    except Exception as error:
+        print("Some error occured during writing to a file: {0}".format(error))
