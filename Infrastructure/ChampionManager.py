945195b89a976b48b3e860ee13e524d108cbe2c5
Michał Kruszewski <kruszewski2@gmail.com>
2019-12-22 20:32:46 +0100

    Edit champion function added

--- Infrastructure/ChampionManager.py
+++ Infrastructure/ChampionManager.py
@@ -7,3 +7,17 @@ def add_champion(new_champion):
 
 def get_champions():
     return championsDB
+
+
+def edit_champion(champion):
+    for champ in championsDB:
+        if champ.id == champion.id:
+            assign_values(champ, champion)
+
+
+def assign_values(champion_from_db, new_champion):
+    champion_from_db.name = new_champion.name
+    champion_from_db.matches = new_champion.matches
+    champion_from_db.souls = new_champion.souls
+    champion_from_db.stats = new_champion.stats
+    champion_from_db.game_time = new_champion.game_time
